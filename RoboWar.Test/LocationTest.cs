e254a22660ddbba7420dc8dbfcdae106051f94a2
Divyesh Bhartiya <divyesh.bhartiya@gmail.com>
2022-10-21 18:36:40 +0530

    test project

---
+++ RoboWar.Test/LocationTest.cs
@@ -0,0 +1,40 @@
+﻿namespace RoboWar.Test;
+
+[TestClass]
+public class RoboWarTest
+{
+    [TestMethod]
+    public void TestScanrio_12N_LMLMLMLMM()
+    {
+        var positonalParams = "1 2 N".Split(' ');
+        var envDimensions = "5 5".Split(' ').Select(int.Parse).ToArray();
+        var commands = "LMLMLMLMM";
+        var location = new Location(positonalParams);
+        
+        var actualOutput = location.FollowCommands(envDimensions, commands.ToUpper()); ;
+        var expectedOutput = "1 3 N";
+
+        Assert.AreEqual(expectedOutput, actualOutput);
+    }
+
+    [TestMethod]
+    public void TestScanrio_33E_MRRMMRMRRM()
+    {
+        var positonalParams = "3 3 E".Split(' ');
+        var envDimensions = "5 5".Split(' ').Select(int.Parse).ToArray();
+        var commands = "MMRMMRMRRM";
+        var location = new Location(positonalParams);
+
+        var actualOutput = location.FollowCommands(envDimensions, commands.ToUpper()); ;
+        var expectedOutput = "5 1 E";
+
+        Assert.AreEqual(expectedOutput, actualOutput);
+    }
+
+    [TestMethod]
+    public void TestScanrio_InvalidDataException()
+    {
+        var positonalParams = new string[] { };
+        Assert.ThrowsException<InvalidDataException>(() => new Location(positonalParams));
+    }
+}
---
+++ RoboWar.Test/ProgramTest.cs
@@ -0,0 +1,31 @@
+﻿namespace RoboWar.Test
+{
+    [TestClass]
+    public class ProgramTest
+    {
+        [TestMethod]
+        public void TestScanrio_Valid()
+        {
+            var positonalParams = "1 2 N".Split(' ');
+            var envDimensions = "5 5".Split(' ').Select(int.Parse).ToArray();
+            var commands = "LMLMLMLMM";
+            var location = new Location(positonalParams);
+
+            var actualOutput = location.FollowCommands(envDimensions, commands.ToUpper()); ;
+            var expectedOutput = "1 3 N";
+
+            Assert.AreEqual(expectedOutput, actualOutput);
+        }
+
+        [TestMethod]
+        public void TestScanrio_Invalid()
+        {
+            var positonalParams = new string[] { };
+            var envDimensions = new string[] { };
+            var commands = string.Empty;
+
+            Assert.ThrowsException<InvalidDataException>(() => Program.Main(new string[] { }));
+        }
+    }
+}
+
